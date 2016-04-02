4d0abf5a877cb1bc238135742beeb38ce73c5952
d3ming <d3ming@gmail.com>
2016-04-01 23:56:01 -0700

    Test hooks to skip login
    Usage: navigate directly to `/app/?userId=<userId>&name=<name&userToke=<token>`

--- app/components/AppContainer.js
+++ app/components/AppContainer.js
@@ -1,9 +1,29 @@
 var React = require('react');
 
 var AppContainer = React.createClass({
+  getInitialState: function() {
+    var query = this.props.location.query,
+        locState = this.props.location.state;
+
+    if (query) {
+      // query param test hooks for userId and name
+      return {
+        name: query.name,
+        userToken: query.userToken,
+        userId: query.userId
+      }
+    }
+
+    return {
+        // Get data from state if possible
+        userId: !!locState.userId ? locState.userId : '',
+        name: !!locState.name ? locState.name : '',
+        userToken: !!locState.userToken ? locState.userToken : ''
+      }
+  },
   render: function() {
     return (<div className="jumbotrol col-sm-12 text-center">
-        <h1>Memento - {this.props.location.state.name}!</h1>
+        <h1>Memento - {this.state.name}!</h1>
     </div>
     );
   }
